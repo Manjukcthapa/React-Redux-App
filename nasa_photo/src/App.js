bec1e8d49f71b741129e341daa5d3369fb487267
Manju KC <kcmanjuthapa@gmail.com>
2019-10-10 17:30:13 -0400

    added header component and changed styling

--- nasa_photo/src/App.js
+++ nasa_photo/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import PhotoList from "./component/PhotoList";
+import Header from "./component/Header"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header/>
         <h1>Photo of the day</h1>
         <PhotoList />
       </div>
