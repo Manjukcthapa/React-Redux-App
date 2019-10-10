98e6f02dae88aa58484b0516522f4bed5090fa99
Manju KC <kcmanjuthapa@gmail.com>
2019-10-10 15:38:05 -0400

    added fetching functions to the reducer

--- nasa_photo/src/App.js
+++ nasa_photo/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import PhotoList from './component/PhotoList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     <h1>hello</h1>
+     <h1>Photo of the day</h1>
+     <PhotoList/>
       </div>
     );
   }
--- nasa_photo/src/store/reducer/index.js
+++ nasa_photo/src/store/reducer/index.js
@@ -1,13 +1,32 @@
-import { START_FETCHING} from '../actions';
+import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from "../actions";
 
 export const initialState = {
-    photo: [""],
+  photo: [""],
   isLoading: false,
   error: ""
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case START_FETCHING:
+      return {
+        ...state,
+        isFetching: true,
+        error: ""
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: "",
+        catFacts: action.payload
+      };
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false
+      };
     default:
       return state;
   }
