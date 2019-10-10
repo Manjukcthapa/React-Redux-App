84d1fe72443ef1165a0df2febb874d29c5ec0c1e
Manju KC <kcmanjuthapa@gmail.com>
2019-10-10 15:33:19 -0400

    created photolist component

--- nasa_photo/src/component/PhotoList.js
+++ nasa_photo/src/component/PhotoList.js
@@ -1,7 +1,10 @@
 import React from "react";
+import PhotoCard from "./PhotoCard";
 
 const PhotoList = () => {
-
+<div>
+{props.photo.map(item => <PhotoCard key={item.name} photo={item} />)}
+</div>
 }
 
 export default PhotoList;
