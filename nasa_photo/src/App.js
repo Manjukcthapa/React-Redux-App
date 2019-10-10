c0c2b7b89ca50f8eb7c168338b89fcce98697fc9
Manju KC <kcmanjuthapa@gmail.com>
2019-10-10 17:50:54 -0400

    changed styling and finished mvp

--- nasa_photo/src/App.js
+++ nasa_photo/src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
 import PhotoList from "./component/PhotoList";
-import Header from "./component/Header"
+import Header from "./component/Header";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+height:1200px;
+width:1000px;
+border:2px solid grey;
+margin-left:120px;
+margin-top:60px;
+display: block;
+background-color: #061f4a;`
+
+const H1 = styled.h1`
+color:white;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header/>
-        <h1>Photo of the day</h1>
+      <Wrapper className="App">
+      
+        <H1>Photo of the day</H1>
         <PhotoList />
+      </Wrapper>
       </div>
     );
   }
--- nasa_photo/src/component/PhotoCard.js
+++ nasa_photo/src/component/PhotoCard.js
@@ -1,12 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+
+const Photo = styled.img`
+  height: 550px;
+  width: 700px;
+  margin-left: 40px;
+  margin-top: 80px;
+  
+`;
+
+const H1 = styled.h1`
+color:white;
+`
+
+const P = styled.p`
+width: 90%;
+font-family: italic;
+margin-left:20px;
+padding: 5px;
+font-size: 22px;
+color:white
+`
 
 const PhotoCard = props => {
   return (
     <div>
-      <h1>{props.photo.date}</h1>
-      <h1>{props.photo.copyright}</h1>
-      <p>{props.photo.explanation}</p>
-      <img src={props.photo.hdurl} alt="" />
+      <Photo src={props.photo.hdurl} alt="" />
+      <H1>{props.photo.date}</H1>
+      <H1>{props.photo.copyright}</H1>
+      <P>{props.photo.explanation}</P>
     </div>
   );
 };
