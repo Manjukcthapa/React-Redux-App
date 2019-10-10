4b2082d809ef656c435acfccafb9124c1aef1f5a
Manju KC <kcmanjuthapa@gmail.com>
2019-10-10 15:09:13 -0400

    added middleware and thunk in index.js

--- nasa_photo/src/index.js
+++ nasa_photo/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./store/reducer";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from "./App";
 import "./index.css";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
